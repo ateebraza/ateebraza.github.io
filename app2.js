79ed0de50268996b5e690bcc1c17c7f59e59f623
Ateeb <ateeb@behtar.co>
2018-07-24 00:43:24 +0530

    Fare and Availibility

---
+++ app2.js
@@ -0,0 +1,35 @@
+(function() {
+	 // intialise firebase
+	 const config={
+		 apiKey:"AIzaSyD24V3KKMhGa9yZ-OXtIhs6HF5TZ9FEMkw",
+		 authDomain:"railgo-7b1df.firebaseapp.com",
+		 databaseURL: "https://railgo-7b1df.firebaseio.com",
+		 storageBucket:"railgo-7b1df.appspot.com",
+	 };
+	 firebase.initializeApp(config);
+	 
+	 //get element
+	 const txtemail=document.getElementById('txtemail');
+	 const txtpassword=document.getElementById('txtpassword');
+	 const btnsignup=document.getElementById('btnsignup');
+	 //add signup event
+	 btnsignup.addEventListener('click',e=> {
+		 // get email and pass
+		 const email=txtemail.value;
+		 const pass=txtpassword.value;
+		 const auth=firebase.auth();
+		 // sign up
+		 const promise=auth.createUserWithEmailAndPassword(email,pass);
+		 promise.catch(e=> console.log(e.message));
+	 });	 
+      // add realtime listener
+	  firebase.auth().onAuthStateChanged(firebaseUser => {
+		  if(firebaseUser)  {
+			  console.log(firebaseUser);
+			  window.location.href='services.html';
+		  }else{
+			  console.log('not logged in');
+		  }
+	  });
+		 
+});
